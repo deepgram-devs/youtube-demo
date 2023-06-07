35e19624729999133b1a4dd93e2a39138f92649e
Luke Oliff <luke@lukeoliff.com>
2023-06-07 22:25:06 +0100

    its all coming together

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,10 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
