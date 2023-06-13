2ce5b23e532963f8ae6479fda09b56679478545d
Luke Oliff <luke@lukeoliff.com>
2023-06-13 21:31:27 +0100

    add github link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { TranscriptionContextProvider } from "@/context/transcription";
 import { ErrorContextProvider } from "@/context/error";
+import {
+  CodeBracketIcon,
+  CodeBracketSquareIcon,
+} from "@heroicons/react/24/outline";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,11 +31,20 @@ export default function RootLayout({
               <div className="w-[48em] gap-2">
                 <Brand />
                 <Errors />
-                <div className="min-h-[24em]">{children}</div>
-              </div>
+                <div>{children}</div>
+              </div>{" "}
             </main>
           </TranscriptionContextProvider>
         </ErrorContextProvider>
+        <div className="absolute top-0 right-0">
+          <a
+            href="https://github.com/lukeocodes/deeptube"
+            target="_blank"
+            className="text-[#13ef95] flex p-4"
+          >
+            <CodeBracketSquareIcon className="w-6 mr-2" /> GitHub
+          </a>
+        </div>
       </body>
     </html>
   );
